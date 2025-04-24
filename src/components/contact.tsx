b1efee06b8037621b1f78ede405d75edc14c3f63
jojosuelobo <josuelobo2000@gmail.com>
2025-04-24 10:41:45 -0300

    fix phone dont exist

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -165,12 +165,12 @@ export default function Contact() {
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-cyan-400 mr-3 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
-                  <div>
+                  {/* <div>
                     <p className="text-sm text-slate-300">Telefone</p>
                     <a href={`tel:${profileData.contact.phone.replace(/\D/g, '')}`} className="text-cyan-300 hover:underline">
                       {profileData.contact.phone}
                     </a>
-                  </div>
+                  </div> */}
                 </li>
                 <li className="flex items-start">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-cyan-400 mr-3 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
